bcd4f2f6d8b9a3e7e33bf4852eefb7b533d1a1b4
Hamza Ashraf <hamza.ashraf@live.co.uk>
2024-09-12 17:47:48 +0100

    set up route, controller function and views to get all of categories

---
+++ controllers/categoriesController.js
@@ -0,0 +1,20 @@
+const db = require('../db/queries');
+
+// function to display all categories
+async function getCategories(req, res) {
+  try {
+    const categories = await db.getAllCategories();
+    console.log(categories)
+    res.render('categories', {     
+      categories : categories 
+    });
+  } catch (error) {
+    console.error('Error fetching categories', error);
+    res.status(500).send('Server Error');
+  }
+}
+
+
+module.exports = {
+  getCategories
+}
---
+++ routes/categoriesRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const categoriesController = require('../controllers/categoriesController');
+const router = express.Router();
+
+
+router.get('/', categoriesController.getCategories);                         // displays all categories
+// router.get('/new', categoriesController.createCategoryGet);               // display  new category form
+// router.post('/new', categoriesController.createCategoryGet);              // save submitted category data
+// router.get('/update/:id', categoriesController.updateCategoryGet);        // displays update form
+// router.post('/update/:id', categoriesController.updateCategoryPost);      // updates category
+// router.post('/delete/:id', categoriesController.deleteCategoryPost )      // deletes category
+
+
+
+
+module.exports = router;
