d6bf9c95947770ea0f7532a5171b0fff55974477
Hamza Ashraf <hamza.ashraf@live.co.uk>
2024-09-12 23:55:55 +0100

    set up crud operations for categories

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -3,12 +3,13 @@ const categoriesController = require('../controllers/categoriesController');
 const router = express.Router();
 
 
-// router.get('/', categoriesController.getCategories);                         displays all categories
+// router.get('/', categoriesController.getCategories);                      // displays all categories
 router.get('/new', categoriesController.createCategoryGet);                  // display  new category form
 router.post('/new', categoriesController.createCategoryPost);                // save submitted category data
 router.get('/update/:id', categoriesController.updateCategoryGet);           // displays update form
 router.post('/update/:id', categoriesController.updateCategoryPost);         // updates category
-router.post('/delete/:id', categoriesController.deleteCategoryPost )      // deletes category
+router.post('/delete/:id', categoriesController.deleteCategoryPost )         // deletes category
+
 // router.get('/:id/items', categoriesController.getItemsByCategories)       // display items in a specific category
 
 
