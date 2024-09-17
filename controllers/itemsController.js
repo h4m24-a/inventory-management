b5d8575f29184ea281d3b3af5a4f4894c56bf832
Hamza Ashraf <hamza.ashraf@live.co.uk>
2024-09-17 18:33:01 +0100

    set up route, controller function and view template to render form for creating a new item

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,5 +1,6 @@
 const db = require('../db/queries');
 
+// function to display all items
 async function getItems(req, res) {
   try {
     const items = await db.getAllItems()
@@ -13,6 +14,19 @@ async function getItems(req, res) {
 }
 
 
+// function to render form to create item
+async function createItemGet(req, res) {
+  try {
+    res.render('items_form');
+  } catch (error) {
+    console.error('Error fetching items', error)
+    throw error
+  }
+  
+}
+
+
 module.exports = {
-  getItems
+  getItems,
+  createItemGet
 }
--- routes/itemsRouter.js
+++ routes/itemsRouter.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 
 router.get('/', itemsController.getItems); // displays all categories
+router.get('/new', itemsController.createItemGet) // display form to create new item
+// router.post('/new', itemsController.createItemPost)   //  saves new submitted data 
 
 module.exports = router;
