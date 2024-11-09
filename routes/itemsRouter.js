a14537c7eb71231bbbf23ee86f68093cd2460331
Hamza Ashraf <hamza.ashraf@live.co.uk>
2024-11-09 00:33:33 +0000

    fix delete route for items

--- routes/itemsRouter.js
+++ routes/itemsRouter.js
@@ -8,7 +8,6 @@ router.get('/new', itemsController.createItemGet) // display form to create new 
 router.post('/new', validateItem, itemsController.createItemPost)   //  Adds new submitted data 
 router.get('/update/:id', itemsController.updateItemsGet)  // displays update form
 router.post('/update/:id', itemsController.updateItemsPost) // Adds updated data
-router.post('/delete/:id', itemsController.deleteItemsPost) // Delete an item  on All Items page
-router.post('/categories/:category_id/items/delete/:id', itemsController.deleteItemsInCategoryPost)  // Delete items in a category   
+router.post('/delete/:id', itemsController.deleteItemsPost) // Delete an item  on All Items page 
 
 module.exports = router;
