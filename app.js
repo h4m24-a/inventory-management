a2e48bfd85636ea948e29cbc6e4c4857555ec9d0
Hamza Ashraf <hamza.ashraf@live.co.uk>
2024-11-13 21:37:36 +0000

    update PORT variable

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 require('dotenv').config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PGPORT;
 let path = require('path');
 const session = require("express-session");
 const passport = require("passport");
